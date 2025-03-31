f3915d52fa957e62ab2bd77efd1da88ea65f9a1c
Seth Kantz <sethjkantz@gmail.com>
2025-03-30 23:28:03 -0400

    Added function to let you navigate to .wav file.

---
+++ software/src/read_wav.m
@@ -0,0 +1,21 @@
+function [data, freq, success] = read_wav()
+%{
+ Function that will let you navigate to and open up a given wave file
+ Returns data, frequency of wav file, and True or False (success)
+%}
+    data = [];
+    freq = 0;
+    success = false;
+
+
+    [filename, pathname] = uigetfile({'*.wav', 'Wav Files';'*.*', 'All Files'}, 'Select a File');
+    if filename ~= 0
+        fullpath = fullfile(pathname, filename);
+        disp(['Selected file: ', fullpath]);
+        [data, freq] = audioread(fullpath);
+        success = true;
+    else
+        disp('No file selected.');
+    end   
+
+end
